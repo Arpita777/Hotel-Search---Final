c7159cba7d82207b9786fb2964bfe5006f2305b2
Arpita777 <42198557+Arpita777@users.noreply.github.com>
2019-12-02 12:40:33 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './component/Header'
+import RestaurantList from './component/RestaurantList'
+import SearchByRestaurantName from './component/SearchByRestaurantName'
+import SearchByCuisineName from './component/SearchByCuisineName'
+
+class App extends Component {
+  state={
+     list:RestaurantList.filter(item => item.RestaurantName!==''),
+     restaurantList:[],
+     cuisinelist:[]
+   }
+
+  setRestaurantList(newList){
+    this.setState({
+      restaurantList:newList
+    })
+  }
+
+  setCuisineList(newList){
+    this.setState({
+      cuisinelist:newList
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header />
+       
+        <SearchByRestaurantName list={this.state.list}
+                                setRestaurantList={(e)=>this.setRestaurantList(e)}
+                                restaurantList={this.state.restaurantList}/>
+         <SearchByCuisineName list={this.state.list}
+                                setCuisineList={(e)=>this.setCuisineList(e)}
+                                cuisinelist={this.state.cuisinelist}
+                               />
+          
+           
+         
+      </div>
+    );
+  }
+}
+
+export default App;
